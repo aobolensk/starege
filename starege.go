9719fbbae37b19ce29d78867b7d4dfbd4657fce5
Arseniy Obolenskiy <gooddoog@student.su>
2022-11-26 15:07:17 +0300

    Add customizable 'rootUrl' parameter

--- starege.go
+++ starege.go
@@ -9,12 +9,14 @@ import (
 
 type Args struct {
 	port     int
+	rootUrl  string
 	serveDir string
 }
 
 func parseArgs() Args {
 	args := new(Args)
 	flag.IntVar(&args.port, "port", 8080, "port number")
+	flag.StringVar(&args.rootUrl, "rootUrl", "starege", "URL to serve main page at")
 	flag.StringVar(&args.serveDir, "serveDir", "", "Directory to serve")
 	flag.Parse()
 	return *args
@@ -23,8 +25,8 @@ func parseArgs() Args {
 func main() {
 	args := parseArgs()
 	if args.serveDir != "" {
-		files := http.StripPrefix("/data/", http.FileServer(http.Dir(args.serveDir)))
-		http.Handle("/data/", files)
+		files := http.StripPrefix("/"+args.rootUrl+"/data/", http.FileServer(http.Dir(args.serveDir)))
+		http.Handle("/"+args.rootUrl+"/data/", files)
 	}
 	ipPort := fmt.Sprintf(":%d", args.port)
 	log.Printf("Listening and serving starege on %s", ipPort)
